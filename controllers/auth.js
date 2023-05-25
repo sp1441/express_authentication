daf6108d8be3041289b43df34cd3869116a4e424
Rome Bell <romebell5@gmail.com>
2023-05-24 18:03:46 -0700

    chore: remove solutions

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/signup", (req, res) => {
-  res.render("auth/signup");
+  return res.render("auth/signup");
 });
 
 router.get("/login", (req, res) => {
-  res.render("auth/login");
+  return res.render("auth/login");
 });
 
 module.exports = router;
