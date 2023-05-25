e388c8385b02d6395682f73bb3d976df2dcbc7b0
Sydney Pogue <spogue1441@gmail.com>
2023-05-24 16:32:39 -1000

    feat: Create env variable and add session and flash middleware

--- server.js
+++ server.js
@@ -2,8 +2,24 @@ require('dotenv').config();
 const express = require('express');
 const layouts = require('express-ejs-layouts');
 const app = express();
+const session = require('express-session');
+const flash = require('connect-flash');
 
-// console.log(SECRET_SESSION);
+// environment variables
+SECRET_SESSION = process.env.SECRET_SESSION;
+// console.log(">>>>>>>>>>", SECRET_SESSION);
+// express_authentication git:(main) ✗ npm run dev
+
+// > express_authentication@1.0.0 dev
+// > nodemon
+
+// [nodemon] 2.0.22
+// [nodemon] to restart at any time, enter `rs`
+// [nodemon] watching path(s): *.*
+// [nodemon] watching extensions: js,mjs,json
+// [nodemon] starting `node server.js`
+// >>>>>>>>>> alldayidreamaboutsoftwareengineering
+// 🎧 You're listening to the smooth sounds of port 3000 🎧
 
 app.set('view engine', 'ejs');
 
@@ -11,6 +27,23 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
+
+app.use(flash());            // flash middleware
+
+app.use(session({
+  secret: SECRET_SESSION,    // What we actually will be giving the user on our site as a session cookie
+  resave: false,             // Save the session even if it's modified, make this false
+  saveUninitialized: true    // If we have a new session, we save it, therefore making that true
+}));
+
+// add passport
+
+app.use((req, res, next) => {
+  console.log(res.locals);
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.get('/', (req, res) => {
   res.render('index');
